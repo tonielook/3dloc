9e50dcabe6a578f39335ac509c47913da1292f3e
tonielook <tonielook@gmail.com>
2021-11-03 14:48:46 +0800

    corrected matlab code for var method

--- run_matlab_var.sh
+++ run_matlab_var.sh
@@ -4,7 +4,7 @@ for pts in 5 10 15 20 25 30 35 40 45
 do
 /home/tonielook/MATLAB/R2021b/bin/matlab \
     -nodisplay -nosplash -nodesktop \
-    -r "nSource = $pts;run('./matlab_codes/rPSF_not_grid_nc/demo_notgrid.m');exit;" 
+    -r "nSource = $pts;run('./matlab_codes/demo_rspsf/demo.m');exit;" 
 done 
 > ../test_output/run_matlab.log 2>&1 &
 set +x
