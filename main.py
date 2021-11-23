f1b713815bf6f0d815ea7b8354ef48d2111bdcc2
tonielook <tonielook@gmail.com>
2021-11-23 14:42:51 +0800

    trying another validation set similar to our test cases

--- main.py
+++ main.py
@@ -28,20 +28,22 @@ def learn_localization(rank,world_size,opt,setup_params):
         training_volume = setup_params['training_volume'] + 1
         # split data to train and validation set        
         tmp_train = np.arange(1,training_volume,1).tolist() 
-        tmp_valid = np.arange(9001,10001,1).tolist()
+        tmp_valid = np.arange(17001,18001,1).tolist()
         # ID 15001~16000 are speical vaildation imgs with possion flux
         # tmp_valid = np.arange(15001,16001,1).tolist()
-        path_train = os.path.join(setup_params['training_data_path'],'train')
-        hardsamples = fnmatch.filter(os.listdir(path_train),'im20000*mat')
-        hs_IDs=[]
-        for hs in hardsamples:
-            hs_IDs.append(int(re.findall('[0-9]+',hs)[0]))
-        hs_num = len(hs_IDs)
-        print(hs_IDs)
-        if hs_num > 0 :
-            del tmp_train[-hs_num:]
-        tmp_train = tmp_train + hs_IDs
-        print(tmp_train)    
+
+        # path_train = os.path.join(setup_params['training_data_path'],'train')
+        # hardsamples = fnmatch.filter(os.listdir(path_train),'im20000*mat')
+        # hs_IDs=[]
+        # for hs in hardsamples:
+        #     hs_IDs.append(int(re.findall('[0-9]+',hs)[0]))
+        # hs_num = len(hs_IDs)
+        # print(hs_IDs)
+        # if hs_num > 0 :
+        #     del tmp_train[-hs_num:]
+        # tmp_train = tmp_train + hs_IDs
+        # print(tmp_train)  
+        #   
         train_IDs = [str(i) for i in tmp_train]
         valid_IDs = [str(i) for i in tmp_valid]
 
