ac3d3f4e50318e5b3c4c29a1a2a6785e34fa5bcb
tonielook <tonielook@gmail.com>
2022-01-07 14:21:23 +0800

    concluded this branch

--- 2_run_matlab_var_gen_test.sh
+++ 2_run_matlab_var_gen_test.sh
@@ -1,8 +1,9 @@
 set -x
 # nohup sh -c '\
 
-echo `date` >> ../test_output/var/result_var.csv
+echo "Begin:" `date` >> ../test_output/var/result_var.csv
 
+# for pts in 40 35 30 25 20 15 10 5 45
 for pts in 5 10 15 20 25 30 35 40 45
 do
 /home/tonielook/MATLAB/R2021b/bin/matlab \
@@ -10,4 +11,6 @@ do
     -r "nSource = $pts;run('./matlab_codes/demo_rspsf/demo.m');exit;" 
 done 
 > ../test_output/run_matlab.log 2>&1 &
+echo "End:" `date` >> ../test_output/var/result_var.csv
+
 set +x
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-/home/tonielook/MATLAB/R2021b/bin/matlab -nodisplay -nosplash -nodesktop \
-            -r "nSource = 70;hs_recall_bar=0.9;run('./matlab_codes/postpro_loc_batch.m');exit;" 
+./2_run_matlab_var_gen_test.sh
+# /home/tonielook/MATLAB/R2021b/bin/matlab -nodisplay -nosplash -nodesktop \
+            # -r "nSource = 70;hs_recall_bar=0.9;run('./matlab_codes/postpro_loc_batch.m');exit;" 
