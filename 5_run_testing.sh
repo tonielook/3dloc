ed4f25701156582b8823764a38caa3caca4bccd4
tonielook <tonielook@gmail.com>
2022-01-27 11:31:01 +0800

    get L5 results

--- 5_run_testing.sh
+++ 5_run_testing.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -x
-# declare -a cps=("121515-nTrain9000-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "121617-nTrain9447-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "121715-nTrain10019-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122012-nTrain10682-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122111-nTrain9232-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122210-nTrain9429-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122310-nTrain9628-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122409-nTrain9809-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122611-nTrain10012-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "122912-nTrain9891-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "123012-nTrain9900-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-#                 "123116-nTrain9900-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
-# )
+declare -a cps=("70L5-012111-nTrain9000-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
+                "60L5-012209-nTrain9000-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
+                "50L5-012510-nTrain9000-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss" 
+)
 # pts=(5 25 45)
 
-declare -a cps=("122912-nTrain9891-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss")
+# declare -a cps=("122912-nTrain9891-lr0.001-Epoch190-batchSize10-D250-cnn_residual/checkpoint_best_loss")
 pts=(5 10 15 20 25 30 35 40 45)
 # pts=(70)
 
@@ -35,11 +26,11 @@ for cp in "${cps[@]}"; do
             --clear_dist=1  \
             --upsampling_factor=2  \
             --scaling_factor=170  \
-            --batch_size=8  \
+            --batch_size=10  \
             --train_or_test='test' 
                     
         /home/tonielook/MATLAB/R2021b/bin/matlab -nodisplay -nosplash -nodesktop \
-            -r "nSource = $pt;hs_recall_bar=0.95;run('./matlab_codes/postpro_loc_batch.m');exit;" 
+            -r "nSource = $pt;hs_recall_bar=1;run('./matlab_codes/postpro_loc_batch.m');exit;" 
     done
         echo "End:" `date` >> ../test_output/postpro_result.csv
 done
